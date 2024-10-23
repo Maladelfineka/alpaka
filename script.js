b20aed1eb7f72d1274cabee32f914573232492ad
Ola <129540485+Maladelfineka@users.noreply.github.com>
2024-10-23 17:39:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+let apake = {
+
+    hair: "default",
+    ears: "default", 
+    eyes: "default", 
+    mouth: "default", 
+    neck: "default", 
+    leg: "default", 
+    accessories: "None", 
+};
+console.log(apake)
+
+let hair = {
+    default:"default.png",
+    curls:"curls.png",
+    bang:"bang.png",
+    short:"short.png",
+    elegant: "elegant.png",
+    quiff: "quiff.png"
+};
+console.log(hair)
